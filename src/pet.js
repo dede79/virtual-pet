1e2c7140907ba35409e90d2783c2af6bc8f305ea
Andrea Hurren <andrea@codesigned.co.uk>
2019-02-25 17:34:50 +0000

    Naming Pets walkthrough done

--- src/pet.js
+++ src/pet.js
@@ -1,3 +1,6 @@
-function Pet() {}
+function Pet(name) {
+  // reminder for myself only: 'this' keyword means whatever instance is created, it belongs to the Pet constructor
+  this.name = name
+};
 
 module.exports = Pet;
