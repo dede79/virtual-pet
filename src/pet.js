246d9fa8f407840a533e954c5e28bd92de108bb7
Andrea Hurren <andrea@codesigned.co.uk>
2019-02-25 21:44:32 +0000

    Gettin older  walkthrough done

--- src/pet.js
+++ src/pet.js
@@ -1,6 +1,12 @@
 function Pet(name) {
-  // reminder for myself only: 'this' keyword means whatever instance is created, it belongs to the Pet constructor
-  this.name = name
+  // reminder for myself only: 'this' keyword means whatever instance is created, it belongs to the Pet constructor:
+  this.name = name;
+  this.age = 0;
 };
+
+Pet.prototype.growUp = function() {
+    this.age += 1;
+  
+  };
 
 module.exports = Pet;
